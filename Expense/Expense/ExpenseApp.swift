2d1bd7a45f73802a7fd53d755a71242ca6d37603
TLiLi HAMDI <tlilios@yahoo.com>
2024-06-15 12:03:47 +0200

    PersonalizedSheet

---
+++ Expense/Expense/ExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseApp.swift
+//  Expense
+//
+//  Created by HTLILI on 15/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
